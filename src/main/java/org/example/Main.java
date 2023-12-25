ddaacc0039aa95f5465fbdff3c8b250d5dcebf49
Sru2003 <srushtipednekar15@gmail.com>
2023-12-25 16:48:00 +0530

    hbm2ddl.auto=create

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,23 +8,7 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Configuration config = new Configuration();
-
         config.addAnnotatedClass(org.example.entities.Student.class);
         SessionFactory sessionFactory= config.buildSessionFactory();
-        Session session= sessionFactory.openSession();
-        try {
-            session.beginTransaction();
-            Student student=new Student();
-            student.setId(5);
-            student.setStudentName("Maggie Smith");
-            session.persist(student);
-            session.getTransaction().commit();
-        }
-        finally {
-            session.close();
-            sessionFactory.close();
-        }
-
-
     }
 }
