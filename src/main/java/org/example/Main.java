1c0d5251db47d37f9dca368dcce3eacd9f32dc0f
Sru2003 <srushtipednekar15@gmail.com>
2023-12-25 10:16:55 +0530

    Used hikari cp for creating EntityManager

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,21 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.example.entities.Student;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf= Persistence.createEntityManagerFactory("jpademo");
+        EntityManagerFactory emf= new HibernatePersistenceProvider().createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(),new HashMap<>());
         EntityManager em= emf.createEntityManager();
         try {
             em.getTransaction().begin();
 
             Student student=new Student();
-            student.setId(1);
-            student.setStudentName("Srushti Pednekar");
+            student.setId(3);
+            student.setStudentName("Sachin Pednekar");
 
             em.persist(student);
             em.getTransaction().commit();
@@ -22,4 +26,4 @@ public class Main {
             em.close();
         }
     }
-}-
+}
